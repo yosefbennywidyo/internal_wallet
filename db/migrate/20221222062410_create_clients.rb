a206e22673d507b0abc4838cbe5dc433f40e1f46
Yosef Benny Widyokarsono <yosebennywidyo@gmail.com>
2022-12-24 12:53:04 +0800

    Update create client migration: - set `code` as custom primary key

--- db/migrate/20221222062410_create_clients.rb
+++ db/migrate/20221222062410_create_clients.rb
@@ -1,30 +1,36 @@
 class CreateClients < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      with_options null: false do
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
         t.string :name
         t.string :email
       end
 
       t.timestamps
+      t.index :code, unique: true
     end
 
     create_table :teams do |t|
-      with_options null: false do
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
         t.string :name
         t.string :email
       end
 
       t.timestamps
+      t.index :code, unique: true
     end
 
     create_table :stocks do |t|
-      with_options null: false do
+      with_options null: false, id: false, primary_key: :code do
+        t.string :code
         t.string :name
         t.string :email
       end
 
       t.timestamps
+      t.index :code, unique: true
     end
   end
 end
